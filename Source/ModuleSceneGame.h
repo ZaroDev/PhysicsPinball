3e63e3834b15e20971196d433152888e03321fc8
Lau <lauraisi2000@gmail.com>
2021-10-12 17:40:35 +0200

    UWU

--- Source/ModuleSceneGame.h
+++ Source/ModuleSceneGame.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "Module.h"
 #include "Globals.h"
+#include "p2List.h"
 
 
 class ModuleSceneGame : public Module
 {
 public:
 	SDL_Texture* tex;
+	SDL_Texture* templateTex;
 	int score;
+
+	p2List<PhysBody*> circles;
+	p2List<PhysBody*> boxes;
+	p2List<PhysBody*> chains;
+
 public:
 	ModuleSceneGame(Application* app, bool start_enabled = true);
 	~ModuleSceneGame();
